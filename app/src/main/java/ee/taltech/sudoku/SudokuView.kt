5f74c9b3463c207cff5b2226ef412c68e1ff14d0
Anton Antonov <Pashtet1234>
2021-02-16 02:35:50 +0200

    test

---
+++ app/src/main/java/ee/taltech/sudoku/SudokuView.kt
@@ -0,0 +1,66 @@
+package ee.taltech.sudoku
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class SudokuView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private var sqrtSize = 3
+    private var size = 9
+
+    private var cellSizePixels = 0F
+
+    private val thickLinePaint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = Color.BLACK
+        strokeWidth = 4F
+    }
+
+    private val thinLinePaint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = Color.BLACK
+        strokeWidth = 2F
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        cellSizePixels = (width / size).toFloat()
+        if (canvas != null) {
+            drawLines(canvas)
+        }
+    }
+
+    private fun drawLines(canvas: Canvas) {
+        canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(), thickLinePaint)
+
+        for (i in 1 until size) {
+            val paintToUse = when (i % sqrtSize) {
+                0 -> thickLinePaint
+                else -> thinLinePaint
+            }
+
+            canvas.drawLine(
+                i * cellSizePixels,
+                0F,
+                i * cellSizePixels,
+                height.toFloat(),
+                paintToUse
+            )
+
+            canvas.drawLine(
+                0F,
+                i * cellSizePixels,
+                width.toFloat(),
+                i * cellSizePixels,
+                paintToUse
+            )
+        }
+    }
+
+}
